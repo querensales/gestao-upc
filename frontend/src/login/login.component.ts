14f0cfb39145aaf01e2bb973e287552b10412ba4
Queren Sales <querenhsales@gmail.com>
2025-01-10 22:53:02 -0300

    instalando angular e componentes

---
+++ frontend/src/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    MatSlideToggleModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
