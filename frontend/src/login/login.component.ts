e25d1d3ff0084ff7d6427b9e9ab9e9130ab1b9d0
Queren Sales <querenhsales@gmail.com>
2025-01-12 21:59:40 -0300

    tela de login

--- frontend/src/login/login.component.ts
+++ frontend/src/login/login.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-login',
   imports: [
-    MatSlideToggleModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
