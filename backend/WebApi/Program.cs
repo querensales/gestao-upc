55fd1e02c2000fc1b5d62216e378d0face37c5d2
Kleber Ribeiro <kleber.ribeiro89@gmail.com>
2025-01-14 14:47:10 -0300

    docker compose

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -21,12 +21,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+Console.WriteLine($"Ambiente: {app.Environment.EnvironmentName}");
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
