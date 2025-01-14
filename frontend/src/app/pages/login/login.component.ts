8f32be746fe57a423d63d5d5e339d8404bf550cd
Queren Sales <querenhsales@gmail.com>
2025-01-14 17:32:14 -0300

    formsModule e ngModel

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 import LoginModel from '../../models/login.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  imports: [FormsModule],
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor() { }
   objetoLogin: LoginModel = new LoginModel();
+  valorDoEmail = '';
+  valorDaSenha: string = '';
 
   entrar(): void {
-    this.objetoLogin.email = 'colocar o valor do input aqui';
-    this.objetoLogin.password = 'colocar o valor do input aqui';
+    this.objetoLogin.email = this.valorDoEmail;
+    this.objetoLogin.password = this.valorDaSenha;
+    console.log(this.objetoLogin)
   }
 
 }
