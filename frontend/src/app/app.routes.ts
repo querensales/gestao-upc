f0c32bb247564d94c6391d03cbfaf9bab6c2bae6
Queren Sales <querenhsales@gmail.com>
2025-01-14 21:44:30 -0300

    criando novo componente

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { EsqueceuSenhaComponent } from './pages/esqueceu-senha/esqueceu-senha.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
 
 
@@ -9,7 +10,8 @@ export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path:'home', component: HomeComponent},
   {path:'home', component: HomeComponent},
-  {path:'esqueceuSenha', component: EsqueceuSenhaComponent}
+  {path:'esqueceuSenha', component: EsqueceuSenhaComponent},
+  {path: 'cadastro', component: CadastroComponent}
 ];
 
 @NgModule({
