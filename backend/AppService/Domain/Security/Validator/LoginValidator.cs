781dc7a0c07f90d3a6ce56363326442c2f4214ad
Kleber Ribeiro <kleber.ribeiro89@gmail.com>
2025-02-06 21:23:57 -0300

    validações de login

---
+++ backend/AppService/Domain/Security/Validator/LoginValidator.cs
@@ -0,0 +1,29 @@
+﻿using AppService.Domain.Security.Request;
+using FluentValidation;
+using Repository;
+
+namespace AppService.Domain.Security.Validator;
+
+public class LoginValidator : AbstractValidator<LoginRequest>
+{
+    private readonly AppDbContext _appDbContext;
+    public LoginValidator(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+
+        RuleFor(l => l.Email)
+            .EmailAddress()
+            .WithMessage("email inválido");
+
+        RuleFor(u => u.Email)
+        .Custom((Email, context) =>
+        {
+            if (!_appDbContext.User.Any(u => u.Email == Email))
+            {
+                context.AddFailure("Email inexistente-.");
+            }
+        });
+    }
+
+
+}
