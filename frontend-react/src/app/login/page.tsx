8414de9ece14c97d8abd5f09fb66302c1ddf9e31
Queren Sales <querenhsales@gmail.com>
2025-06-13 20:53:45 -0300

    - style: tela de login com ant desing

--- frontend-react/src/app/login/page.tsx
+++ frontend-react/src/app/login/page.tsx
@@ -1,7 +1,70 @@
+'use client'
+
+import { Button, Checkbox, Col, Form, FormProps, Input, Row } from "antd";
+
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
+
+const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+  console.log('Success:', values);
+};
+
+const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+
+
+
 export default function Autenticacao() {
-  return(
-    <div>
-        
-    </div>
+
+  return (
+    <>
+      <Row>
+        <Col span={8}></Col>
+        <Col span={8}>
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item<FieldType>
+              label="Nome de Usuário"
+              name="username"
+              rules={[{ required: true, message: 'Preencha o usuário corretamente!' }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              label="Senha"
+              name="password"
+              rules={[{ required: true, message: 'Preencha a senha corretamente!' }]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item<FieldType> name="remember" valuePropName="checked" label={null}>
+              <Checkbox>Lembrar-me</Checkbox>
+            </Form.Item>
+
+            <Form.Item label={null}>
+              <Button type="primary" htmlType="submit">
+                Entrar
+              </Button>
+            </Form.Item>
+          </Form></Col>
+        <Col span={8}>
+
+        </Col>
+      </Row>
+    </>
   );
 }
--- frontend-react/src/app/page.tsx
+++ frontend-react/src/app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
-  return (
-    <div>
-        <h1>page 1</h1>
-    </div>
-  );
-}
+import React from 'react';
+import {Col, Row, Space } from 'antd';
+
+const Home = () => (
+<>
+  <Row>
+      <Col span={11}>col</Col>
+      <Col span={2}>meio</Col>
+      <Col span={11}>col</Col>
+    </Row>
+</> 
+);
+
+export default Home;
