e4aa5bfd814eff36c1e38eb818ee83bbaf6313ea
Queren Sales <querenhsales@gmail.com>
2025-06-30 16:31:10 -0300

    ✨ NOVOS COMPONENTES
    Componente de boas vindas e acesso rápido

---
+++ frontend-react/src/app/components/boasVindas/boasVindas.tsx
@@ -0,0 +1,5 @@
+export default function boasVindas() {
+    return(
+        <h1>Boa tarde!</h1>
+    )
+}
---
+++ frontend-react/src/app/components/visaogeral/visaoGeral.tsx
@@ -0,0 +1,10 @@
+import BoasVindas from "../boasVindas/boasVindas";
+
+export default function VisaoGeral() {
+    return (
+        <div>
+            <BoasVindas />
+            container com o despesas e receitas mensais e acesso rápido
+        </div>
+    );
+}
