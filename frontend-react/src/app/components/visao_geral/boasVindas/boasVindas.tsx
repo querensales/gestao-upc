ab0af015a47744f2ccf59fadeed4412f7d99deb6
Queren Sales <querenhsales@gmail.com>
2025-07-17 11:11:26 -0300

    ✨ Criando objetos
    criando objetos em visaoGeral

--- frontend-react/src/app/components/visao_geral/boasVindas/boasVindas.tsx
+++ frontend-react/src/app/components/visao_geral/boasVindas/boasVindas.tsx
@@ -1,22 +1,31 @@
 import Styles from './boasVindas.module.css'
 
 export default function boasVindas() {
+    const informacoes = {
+        nomeUsuario: 'Queren',
+        receitaTotal: 10000,
+        despesaTotal: 5000,
+    }
+
+
+
+
     return (
         <section className={Styles.section}>
             <div className={Styles.ola}>
                 <p>Olá,</p>
-                <h2>Queren!</h2>
+                <h2>{informacoes.nomeUsuario}!</h2>
             </div>
 
             <div className={Styles.mesAtual}>
                 <div>
                     <p>Receitas no mês atual</p>
-                    <p>R$ 10.000,00</p>
+                    <p>R$ {informacoes.receitaTotal}</p>
                 </div>
                 <span className={Styles.divisor}>|</span>
                 <div className={Styles.mesAtual}>
                     <p>Despesas no mês atual</p>
-                    <p>R$ - 5.000,00</p>
+                    <p>R$ {informacoes.despesaTotal}</p>
                 </div>
             </div>
         </section>
